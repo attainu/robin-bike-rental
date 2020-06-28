864287722d4e08d00e00abbaa0035e1cc6efd824
Sai Charan <polavarapucharan@hotmail.com>
2020-06-28 20:22:19 +0530

    commit

--- version1/routes/bookingRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const booking  = require('../controller/bookingController');
-
-router.post('/bike/:id',booking.newBooking);
-
-module.exports = router;
